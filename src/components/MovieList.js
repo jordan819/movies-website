71c0f8474efc909f0f1a64186b99968a1efb81e6
jordan819 <patryk.zaucha00@gmail.com>
2022-01-25 11:17:39 +0100

    added blank photo for missing movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,7 +11,10 @@ const MovieList = (props) => {
 
           <Link to={`/details/${movie.id}`}>
             <div class="img__wrap">
-              <img class="img__img" src={movie.image} alt='movie' width="300" height="400"/>
+              <img class="img__img" src={movie.image} onError={({ currentTarget }) => {
+    currentTarget.onerror = null; // prevents looping
+    currentTarget.src="https://image.shutterstock.com/image-vector/photo-coming-soon-vector-image-600w-1809858361.jpg";
+  }} alt='movie' width="300" height="400"/>
               <p class="img__description">{movie.content}</p>
             </div>
           </Link>
