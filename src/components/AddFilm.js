12871f4b7c4f2f5969fd04694d2f3681a800e036
jordan819 <patryk.zaucha00@gmail.com>
2022-01-25 10:59:03 +0100

    added addFilm functionality

--- src/components/AddFilm.js
+++ src/components/AddFilm.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import Heading from './Heading';
+const axios = require('axios');
 
 const AddFilm = () => {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  // const navigate = useNavigate();
+
+  const addFilm = () => {
+
+    if(title.trim() === '' || description.trim() === '' || imageUrl === '') {
+      console.log('Missing movie info!');
+      return;
+    };
+
+    console.log('title: ' + title);
+    console.log('description: ' + description);
+    console.log('imageUrl: ' + imageUrl);
+
+
+    axios({
+      method: 'post',
+      url: 'https://pr-movies.herokuapp.com/api/movies',
+      data: {
+        title: title,
+        image: imageUrl,
+        content: description
+      }
+    }).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.log('Mam Error: ' + error);
+    })
+
+  };
+
   return (
     <div>
 
@@ -10,87 +46,33 @@ const AddFilm = () => {
         <Heading/>
       </Link>
 
-
       <div style={{padding: '50px'}}>
         <div style={{textAlign: 'center'}}>
           <h2> Dodawanie filmu </h2>
         </div>
 
         <form>
-
           <div class="form-group w-50 mt-4 mb-4">
             <label for="titleInput">Tytuł filmu</label>
-            <input type="text" class="form-control" id="titleInput" placeholder="Tytuł"/>
+            <input type="text" class="form-control" id="titleInput" placeholder="Tytuł" onChange={(event) => setTitle(event.target.value)} value={title}/>
           </div>
 
           <div class="form-group w-50  mt-4 mb-4">
-            <label for="directorInput">Reżyser</label>
-            <input type="text" class="form-control" id="directorInput" placeholder="Reżyser"/>
+            <label for="directorInput">Opis</label>
+            <input type="text" class="form-control" id="descriptionInput" placeholder="Opis" onChange={(event) => setDescription(event.target.value)} value={description}/>
           </div>
 
           <div class="form-group w-50  mt-4 mb-4">
-            <label for="actorsInput">Aktorzy</label>
-            <input type="text" class="form-control" id="actorsInput" placeholder="Aktorzy"/>
+            <label for="actorsInput">Zdjęcie</label>
+            <input type="text" class="form-control" id="imageUrlInput" placeholder="Link do zdjęcia" onChange={(event) => setImageUrl(event.target.value)} value={imageUrl}/>
           </div>
-
-          <div class="form-group w-50  mt-4 mb-4">
-            <label for="languageInput">Język</label>
-            <input type="text" class="form-control" id="languageInput" placeholder="Język"/>
-          </div>
-
-          <p>Kategoria</p>
-          <div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre1" value="action"/>
-              <label class="form-check-label" for="inlineCheckbox1">Akcja</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre2" value="animation"/>
-              <label class="form-check-label" for="inlineCheckbox2">Animacja</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre3" value="biography"/>
-              <label class="form-check-label" for="inlineCheckbox2">Biografia</label>
-            </div>
-          </div>
-          <div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre4" value="document"/>
-              <label class="form-check-label" for="inlineCheckbox2">Dokument</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre5" value="drama"/>
-              <label class="form-check-label" for="inlineCheckbox2">Dramat</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre6" value="comedy"/>
-              <label class="form-check-label" for="inlineCheckbox2">Komedia</label>
-            </div>
-          </div>
-          <div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre7" value="criminal"/>
-              <label class="form-check-label" for="inlineCheckbox2">Kryminał</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre8" value="thriller"/>
-              <label class="form-check-label" for="inlineCheckbox2">Thriller</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" id="genre9" value="western"/>
-              <label class="form-check-label" for="inlineCheckbox2">Western</label>
-            </div>
-          </div>
-
-          <button style={{backgroundColor: '#d30f0f', color: '#fff', marginTop: '20px'}} type="submit" class="btn">Dodaj</button>
-
-          <Link to="/">
-            <button style={{marginTop: '20px', marginLeft: '20px'}} type="button" class="btn btn-secondary">Powrót</button>
-          </Link>
-
-
-
         </form>
+        <button onClick={addFilm} style={{backgroundColor: '#d30f0f', color: '#fff', marginTop: '20px'}} type="submit" class="btn">Dodaj</button>
+
+        <Link to="/">
+          <button style={{marginTop: '20px', marginLeft: '20px'}} type="button" class="btn btn-secondary">Powrót</button>
+        </Link>
+
       </div>
 
     </div>
